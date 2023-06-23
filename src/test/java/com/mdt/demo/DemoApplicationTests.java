869d357d31267f88cde03cfd789de5a352670d74
Jose De Paz <jdepaz@hexacta.com>
2023-06-23 16:05:33 -0500

    base line

---
+++ src/test/java/com/mdt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
